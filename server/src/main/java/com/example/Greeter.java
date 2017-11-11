b3561a560ce2706920e02d2670163eb591f1c283
Rajesh Garigipati <rajesh.garigipati@gmail.com>
2017-11-11 14:31:41 -0800

    fixed warnings 2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,8 @@ public class Greeter {
   }
 
   /**
-   *
+   * @param blah
+   * @return string
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
